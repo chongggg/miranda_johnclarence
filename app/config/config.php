82087a51ea4986f3a3f484bf76532dc9af0ddccb
Clarence M. Miranda <johnclarencemiranda382@gmail.com>
2025-09-19 12:39:33 +0800

    First commit

--- app/config/config.php
+++ app/config/config.php
@@ -298,3 +298,5 @@ $config['csrf_cookie_name']        = 'csrf_cookie_name';
 $config['csrf_expire']             = 7200;
 $config['csrf_regenerate']         = FALSE;
 ?>
+
+$config['sess_save_path'] = sys_get_temp_dir();
